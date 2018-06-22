b0c5c5a2799f2dadb2b852e862e1097696189ebb
Steve Vlaminck <steve@vlaminck.com>
2018-06-22 10:37:23 -0500

    Added Log.swift

---
+++ Log.swift
@@ -0,0 +1,67 @@
+//
+//  Log.swift
+//
+//  Created by Steven Vlaminck on 8/18/16.
+//
+
+import Foundation
+
+var logLevel: LogLevel = .debug
+
+enum LogLevel: Int {
+    case debug = 0
+    case info
+    case warn
+    case error
+}
+
+extension LogLevel: Comparable {}
+func ==(lhs: LogLevel, rhs: LogLevel) -> Bool {
+    return lhs.rawValue == rhs.rawValue
+}
+func >(lhs: LogLevel, rhs: LogLevel) -> Bool  {
+    return lhs.rawValue > rhs.rawValue
+}
+func <(lhs: LogLevel, rhs: LogLevel) -> Bool  {
+    return lhs.rawValue < rhs.rawValue
+}
+
+struct Log {
+    static func custom(level: LogLevel, prefix: String, message: Any) {
+        guard logLevel <= level else { return }
+        print("\(prefix) \(message)")
+    }
+    static func debug(_ message: Any) {
+        guard logLevel <= LogLevel.debug else { return }
+        print("🐛 [DEBUG] \(message)")
+    }
+    static func info(_ message: Any) {
+        guard logLevel <= LogLevel.debug else { return }
+        print("🗣 [INFO] \(message)")
+    }
+    static func warn(_ message: Any) {
+        guard logLevel <= LogLevel.warn else { return }
+        print("💥 [WARN]  \(message)")
+    }
+    static func error(_ message: Any) {
+        guard logLevel <= LogLevel.error else { return }
+        print("💩 [ERROR] \(message)")
+    }
+}
+
+func 🐛(_ message: Any) {
+    Log.debug(message)
+}
+func 🗣(_ message: Any) {
+    Log.info(message)
+}
+func 💥(_ message: Any) {
+    Log.warn(message)
+}
+func 💩(_ message: Any) {
+    Log.error(message)
+}
+
+func 🏅(_ message: Any) {
+    Log.custom(level: .info, prefix: "🏅 [SUCCESS]", message: message)
+}
